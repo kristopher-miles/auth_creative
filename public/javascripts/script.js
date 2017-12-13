4685e303c85b4763e83fe27c7847767a62e8ffcb
Kristopher Miles <kristophermiles@gmail.com>
2017-12-12 20:33:30 -0700

    A few improvements to database flow.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,30 +5,27 @@ var provider = new firebase.auth.GoogleAuthProvider();
 myApp.controller("chatController", ["$scope", "$firebaseArray",
                                     function($scope, $firebaseArray) {
    var ref = firebase.database().ref().child("messages");
+                                        
    $scope.chats = $firebaseArray(ref);
    $scope.update = function(user) {
-       var newmessage = {from:user.name || "anonymous",body:user.chat};
+       var newmessage = {from:firebase.auth().currentUser.displayName,image:firebase.auth().currentUser.photoURL, body:user.chat};
        console.log(newmessage);
        $scope.chats.$add(newmessage);
        user.chat = "";
    }
-   
-   
-   
  }
                                     
 ]);
 
 function login(){
     firebase.auth().signInWithRedirect(provider);
+    verifyLoggin();
 }
 
 function logout(){
-        firebase.auth().signOut().then(function() {
-  console.log("This user has signed out successfully");
-}).catch(function(error) {
-  // An error happened.
-});
-        
-    }
+    firebase.auth().signOut().then(function() {
+        console.log("This user has signed out successfully");
+        verifyLoggin();
+    }).catch(function(error) {});      
+}
 
